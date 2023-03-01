e97c3cdf14fab0654ca286b368f2fd61535dd07b
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-01 22:19:40 +0700

    feat: add setter and default ctor angka warna

--- src/Card/DeckCard.cpp
+++ src/Card/DeckCard.cpp
@@ -13,13 +13,13 @@ void DeckCard::printInfo()
 }
 
 int Angka::getNum(){return this->num;}
-
+Angka::Angka(){}
 Angka::Angka(int num){
     this->num = num;
 }
-
+Warna::Warna(){}
 Warna::Warna(int t){
-    this->type = type;
+    this->type = t;
 }
 int Warna::getType()
 {
@@ -33,23 +33,29 @@ DeckCardCollection::DeckCardCollection(){
         for (int j = 0 ; j < 13; j++)
         {
             DeckCard* c = new DeckCard(i+1,j+1);
+            this->buffer.push_back(*c);
         }
-        this->buffer.push_back(*c);
     }
 
     /* Mengacak */
     vector<DeckCard> acak;
     for(int i = 0; i < 52; i++){
-        DeckCard* c = this->buffer[Rand() % 52];
-        while(find(acak.begin(), acak.end(), *c) != acak.end()){
-            *c = this->buffer[Rand() % 52];
-        }
-        acak.push_back(*c);
+        DeckCard c = this->buffer[rand() % 52];
+        /* !!! INI BIKIN ERROR BRO !!!!*/
+        // while(find(acak.begin(), acak.end(), c) != acak.end()){
+        //     c = this->buffer[rand() % 52];
+        // }
+        acak.push_back(c);
     }
 }
+
 
 DeckCardCollection::~DeckCardCollection(){}
 
 DeckCard DeckCardCollection::getCard(int i){
     return(this->buffer[i]);
+}
+
+void DeckCardCollection::setCard(DeckCard& c){
+    this->buffer.push_back(c);
 }
