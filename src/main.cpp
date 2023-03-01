a3c47033afa2460d20e421eb412f85bffe9636c7
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-01 23:34:28 +0700

    feat: main function draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
-#include "Card/AbilityCard.hpp"
-#include "Card/Card.hpp"
+// #include "Card/AbilityCard.hpp"
+#include "Card/DeckCard.hpp"
 #include "GameManager/GameManager.hpp"
 #include "Player/Player.hpp"
-#include "Rules/Rules.hpp"
 
 int main()
 {
-    GameManager* newGame = new GameManager();
+    GameManager* game = new GameManager();
+    int round = 1;
+    while(true){
+        if (round == 1){
+            // for each player give 2 cards from table cards
+            for (int i = 0 ; i < 7 ; i++){
+                game->players.getPlayer(i)+game->tableCards.takeCard();
+                game->players.getPlayer(i)+game->tableCards.takeCard();
+            }
+
+            for (int i = 0 ; i < 7 ; i++){
+                cout<<"Tentukan aksi anda: "<<endl;
+                cout<<"1. Next"<<endl;
+                cout<<"2. Double"<<endl;
+                cout<<"3. Half"<<endl;
+                string aksi;
+                cin>>aksi;
+            }
+
+            // Give 1 ability card to each player
+            /* TODO */
+        }
+
+        else if(round >= 2 && round <= 6){
+            game->playCards+game->tableCards.takeCard();
+
+            for (int i = 0 ; i < 7 ; i++){
+                cout<<"Tentukan aksi anda: "<<endl;
+                cout<<"1. Next"<<endl;
+                cout<<"2. Double"<<endl;
+                cout<<"3. Half"<<endl;
+                string aksi;
+                cin>>aksi;
+            }
+        }
+
+        round++;
+    }
     return 0;
 }
