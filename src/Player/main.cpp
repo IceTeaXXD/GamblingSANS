e6be10a04f109cf53cc86287bfe111decd3fbc3e
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-01 22:20:42 +0700

    feat: update main

--- src/Player/main.cpp
+++ src/Player/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include "Player.hpp"
-#include "../Card/AbilityCard.hpp"
+// #include "../Card/AbilityCard.hpp"
 #include "../Card/DeckCard.hpp"
 using namespace std;
 
 int main()
 {
     Player* player1 = new Player("NABIL");
-    DeckCard* satukuning = new DeckCard(3,1);
-    // cout<<"num : " << satukuning->getNum() << endl;
-    // cout<<"type : " << satukuning->getType() << endl;
-    //INI KNP GABISA DI ASSIGN DAH
-    //MUNCULNYA ALAMAT MULU
-    player1->addCard(*satukuning);
+    DeckCard* satukuning = new DeckCard(3,2);
+    satukuning->printInfo();
+    *player1+*satukuning;
     player1->viewAllCard();
     return 0;
 }
