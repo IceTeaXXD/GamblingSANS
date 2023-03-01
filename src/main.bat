144bbdca5d8c96567e326aa659553594aff6b939
henryanandsr <13521004@std.stei.itb.ac.id>
2023-03-02 01:20:39 +0700

    add main.bat

--- src/main.bat
+++ src/main.bat
@@ -1,2 +1,9 @@
-g++ -o main main.cpp Card/DeckCard.cpp GameManager/GameManager.cpp Player/Player.cpp Player/ArrOfPlayer.cpp
-.\main
+g++ -c "./Card/AbilityCard.cpp" -o AbilityCard.o
+g++ -c "./Card/DeckCard.cpp" -o DeckCard.o
+g++ -c "./GameManager/GameManager.cpp" -o GameManager.o
+g++ -c "./Player/Player.cpp" -o Player.o
+g++ -c "./Player/ArrOfPlayer.cpp" -o ArrOfPlayer.o
+g++ -c "./Rules/FindValue.cpp" -o FindValue.o
+g++ -c "./Rules/Kombinasi.cpp" -o Kombinasi.o
+g++ main.cpp AbilityCard.o DeckCard.o GameManager.o Player.o ArrOfPlayer.o FindValue.o Kombinasi.o
+a.exe
