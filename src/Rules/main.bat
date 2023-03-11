71bd3c0a6b00a0d7cb55904571653309becc0b24
MHEN2606 <matthew.mahendra@gmail.com>
2023-03-11 09:40:33 +0700

    Benerin Kombinasi.cpp

--- src/Rules/main.bat
+++ src/Rules/main.bat
@@ -4,6 +4,8 @@ g++ -c "../Card/DeckCard.cpp" -o DeckCard.o
 g++ -c "../Card/CardCollection.cpp" -o CardCollection.o
 g++ -c "../Card/Warna.cpp" -o Warna.o
 g++ -c "../Card/Angka.cpp" -o Angka.o
-g++ main.cpp DeckCard.o FindValue.o Kombinasi.o Warna.o Angka.o CardCollection.o
+g++ -c "../Card/Abilities.cpp" -o Abilities.o
+g++ -c "../Card/AbilityCard.cpp" -o AbilityCard.o
+g++ main.cpp AbilityCard.o DeckCard.o FindValue.o Kombinasi.o Warna.o Angka.o CardCollection.o Abilities.o
 test&cls
 a.exe
